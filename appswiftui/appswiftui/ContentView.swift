f7a585e1fb2defbe8a9c89a55410369bfcac2354
kellerlivia <rm94275@fiap.com.br>
2024-02-21 19:40:48 -0300

    criando app

---
+++ appswiftui/appswiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  appswiftui
+//
+//  Created by Livia Keller on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ appswiftui/appswiftui/appswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  appswiftuiApp.swift
+//  appswiftui
+//
+//  Created by Livia Keller on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct appswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
