6a71a45f39aaf3d71e7d7a264e63460a1431819e
kellerlivia <rm94275@fiap.com.br>
2024-02-26 21:45:02 -0300

    datePicker e Toggle

---
+++ appswiftui/appswiftui/ProgressViewScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressViewScreen.swift
+//  appswiftui
+//
+//  Created by Livia Keller on 26/02/24.
+//
+
+import SwiftUI
+
+struct ProgressViewScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressViewScreen()
+}
