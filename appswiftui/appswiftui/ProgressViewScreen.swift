b9d168add0c43048418bf598ed500546e6811b42
kellerlivia <rm94275@fiap.com.br>
2024-02-26 21:46:14 -0300

    progressView e stepper

--- appswiftui/appswiftui/ProgressViewScreen.swift
+++ appswiftui/appswiftui/ProgressViewScreen.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ProgressViewScreen: View {
+    
+    @State var progress: Double = 0
+    var maxValue: Double = 10
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            Button("advance") {
+                progress += 0.5
+            }
+            
+            ProgressView("loading...",
+                         value: progress,
+                         total: maxValue)
+            .labelsHidden()
+            .padding()
+            
+            Button("decrease") {
+                startProgress()
+            }
+        }
+        .padding()
+    }
+    
+    func startProgress() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            if self.progress < maxValue {
+                progress += 2
+            } else {
+                timer.invalidate()
+                progress = 0
+            }
+        }
     }
 }
 
--- appswiftui/appswiftui/ToggleScreen.swift
+++ appswiftui/appswiftui/ToggleScreen.swift
@@ -10,16 +10,25 @@ import SwiftUI
 struct ToggleScreen: View {
     
     @State var isOn: Bool = true
+    @State var count: Int = 0
     
     var body: some View {
         ZStack {
             Color(isOn ? .pink : .purple)
                 .ignoresSafeArea(.all)
-            VStack {
+            VStack(spacing: 20.0) {
                 Toggle(isOn: $isOn, label: {
                     Text("Test")
                 })
                 .labelsHidden()
+                
+                Stepper("contador",
+                        value: $count,
+                        in: 0...10,
+                        step: 2)
+                .labelsHidden()
+                
+                Text("value: \(count)")
             }
             .padding()
         }
